07e2b24cc2de66ab7b7dac201713f5d5fde6bc2b
Valentin <vpavonlopez@gmail.com>
2024-01-23 09:26:39 +0100

    node

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+require('dotenv').config()
+const app =  express()
+
+app.get('/',(req,res)=>{
+    res.send(`${process.env.PORT}\n${process.env.MYSQL_USER}`)
+})
+
+
+
+app.get('/about',(req,res)=>{
+    res.send('<h1>Acerca de...</h1>')
+})
+
+
+
+let PORT = process.env.PORT || '3000'
+
+app.listen( PORT,(req,res)=>{
+    console.log(`server running on port ${PORT}`);
+})
